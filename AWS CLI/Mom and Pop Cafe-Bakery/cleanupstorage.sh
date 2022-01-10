fdc646e68343318a387a3e0ebe109a9213c866f6
Georges034302 <georges.boughantous@gmail.com>
2022-01-11 10:36:10 +1100

    s3 cli scripts

--- AWS CLI/Mom and Pop Cafe-Bakery/cleanupstorage.sh
+++ AWS CLI/Mom and Pop Cafe-Bakery/cleanupstorage.sh
@@ -3,6 +3,6 @@
 echo -n "Bucket name: "
 read bucket
 
-aws s3 rm s3://$bucket --recursive
+aws s3 rm s3://$bucket --recursive  #deletes the contents of the bucket (empty)
 
-aws s3 rb s3://$bucket
+aws s3 rb s3://$bucket #deletes the bucket itself
