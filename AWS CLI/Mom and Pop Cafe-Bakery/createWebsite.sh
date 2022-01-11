109c7f9a89e83dbdf2b071e66613099fbe5b1fa2
Georges034302 <georges.boughantous@gmail.com>
2022-01-11 12:21:58 +1100

    s3 cli scripts

--- AWS CLI/Mom and Pop Cafe-Bakery/createWebsite.sh
+++ AWS CLI/Mom and Pop Cafe-Bakery/createWebsite.sh
@@ -7,6 +7,10 @@ read bucket
 
 aws s3 mb s3://$bucket
 
+aws s3 website s3://$bucket/ --index-document index.html
+
 aws s3 sync . s3://$bucket --acl public-read
 
 aws s3 ls
+
+curl http://$bucket.s3-website.ap-southeast-2.amazonaws.com
